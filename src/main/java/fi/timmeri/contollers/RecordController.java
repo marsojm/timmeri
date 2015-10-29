0b715682e673e52530c6d118bfee5f148a2d5079
Marko Malinen <markojm1986@gmail.com>
2015-10-29 18:59:08 +0200

    Enabled spring security

--- src/main/java/fi/timmeri/contollers/RecordController.java
+++ src/main/java/fi/timmeri/contollers/RecordController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("api/")
 @RestController
 public class RecordController {
     private static final Logger log = LoggerFactory.getLogger(RecordController.class);
